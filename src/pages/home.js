2ace436c62d19449d0f1f8727af3105c0dd0d0e0
NLJZ <johnson.nathan.lee@gmail.com>
2020-03-09 16:06:41 +0100

    small updates to site structure

---
+++ src/pages/home.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./home-styles.css";
+
+class Home extends Component {
+  render() {
+    return (
+      <div class="home">
+        <div class="splashcontainer">
+          <div id="splashimage">
+            <img src="../images/purpleear.jpg" alt="Ear" />
+          </div>
+
+          <div id="splashmenu">
+            <h1>ELENA</h1>
+            <h1>ZIESER</h1>
+            <ul>
+              <li>
+                <a href="work.html">work</a>
+              </li>
+              <li>
+                <a href="news.html">news</a>
+              </li>
+              <li>
+                <a href="about.html">about</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
