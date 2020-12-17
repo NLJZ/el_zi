0622b70732270317e97298fa9f662cb750a7484e
NLJZ <johnson.nathan.lee@gmail.com>
2020-12-17 13:38:57 +0100

    conditionally render images

--- src/components/work/Project.js
+++ src/components/work/Project.js
@@ -8,9 +8,18 @@ function Project(props) {
   const project = Data.filter((item) => item.id === projectId)[0];
   const description = parse(project.description);
   const credits = parse(project.credits);
+  console.log(project);
+  const Image = () => {
+    if (project.image !== undefined || null) {
+      return <img src={project.image} alt={project.title + "image"} />;
+    } else {
+      return null;
+    }
+  };
   return (
     <div className="project">
       <h1>{project.title}</h1>
+      <Image />
       {description}
       {credits}
     </div>
